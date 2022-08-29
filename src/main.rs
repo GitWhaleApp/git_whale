3c7ef5dbc9b22336d07965fb41dd3515a9221798
Rudrabhoj Bhati <hello@rudrabhoj.com>
2022-08-29 09:08:12 +0530

    Changing version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn get_version() -> String {
+    String::from(env!("CARGO_PKG_VERSION"))
 }
+
+
+fn main() {
+    println!("Welcome to GitWhale v{}", get_version());
+}
+
